906fb8a10f49a431d87e0644e98c09a52fa5f0e9
Oleksii Kuznietsov <utilmind@gmail.com>
2025-11-13 19:22:32 -0500

    spaces

--- db-dump.bat
+++ db-dump.bat
@@ -35,7 +35,7 @@ REM ============================================================================
 
 REM ================== CONFIG ==================
 REM Path to bin folder containing mysql/mysqldump or mariadb/mariadb-dump. (Optionally. Something like "SQLBIN=C:\Program Files\MariaDB 10.5\bin".)
-REM ATTN! If you have MULTIPLE VERSIONS of MySQL/MariaDB installed on your computer, please specify the exact path to the working version you are dumping from!
+REM ATTN! If you have MULTIPLE INSTALLATIONS of MySQL/MariaDB on your computer, please specify the exact path to the working version you are dumping from!
 set "SQLBIN="
 REM Run "mysql --version" to figure out the version of your default mysql.exe
 REM set "SQLCLI=mariadb.exe"
